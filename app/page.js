771eb81432c77104eff775f2039d5e9e2783fac4
shhady <shhadyse@gmail.com>
2024-08-09 02:15:08 +0300

    first

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div className="max-h-[100dvh]">
     <CompareDemo />
     <Social />
-    </>
+    </div>
   );
 }
