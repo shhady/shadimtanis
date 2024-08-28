0a74c7bf34984ab7fa5d25f940187e5404c70a0c
shhady <shhadyse@gmail.com>
2024-08-28 14:03:58 +0300

    phone ltr

--- app/(pages)/rojeh/page.jsx
+++ app/(pages)/rojeh/page.jsx
@@ -17,7 +17,7 @@ export default function page() {
         <div style={{ position: 'absolute', bottom: 40, right: 20, fontSize: '18px', color:'white' }}>
         רוג'ה נדאף
           <br />
-          ייעוץ / שיווק נדל"ן - חיפה
+          ייעוץ / שיווק נדל&quot;ן - חיפה
         </div>
         {/* <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} /> */}
         {/* <a style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }} href="#">
